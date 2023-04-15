be715a725a31d3690f52f834f3678c73f7617635
suscoder11 <119058788+suscoder11@users.noreply.github.com>
2023-04-15 17:21:18 +0200

    Add files via upload

---
+++ the quantum scheuler V0.0/ProFCFS.h
@@ -0,0 +1,28 @@
+#include "processor.h"
+#pragma once
+class ProFCFS: public processor
+{
+private:
+	LinkedList Plist;
+
+public:
+
+	ProFCFS(scheduler* p);
+
+	virtual ~ProFCFS();
+
+	virtual  int gettimer() const override;
+
+	virtual bool ScheduleAlgo() override;
+
+	int forkingrequest(int AT);
+
+	virtual void inctimer() override ;
+	virtual void dectimer() override;
+
+
+
+
+
+};
+
---
+++ the quantum scheuler V0.0/ProSJB.cpp
@@ -0,0 +1,30 @@
+#include "ProSJB.h"
+
+
+
+
+ProSJB::ProSJB(scheduler* p) :processor(p) 
+{
+	timer = 0;
+
+
+}
+
+
+ProSJB::~ProSJB()
+{}
+
+
+int ProSJB::gettimer() const
+{}
+
+
+bool ProSJB::ScheduleAlgo()
+{}
+
+
+void ProSJB::inctimer()
+{}
+
+void ProSJB::dectimer()
+{}
